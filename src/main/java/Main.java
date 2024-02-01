b380f02e5416436fbaeb27164a0ebee5e9146c36
srinu2003 <88500103+srinu2003@users.noreply.github.com>
2024-02-01 16:21:51 +0000

    Initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,60 @@
 import java.util.Arrays;
+import java.util.Scanner;
+
+// H(x) = (f0(x) + f1(x) + f2(x) + ...) mod m)
+// m : Size
 
 public class Hashmap {
-    private final int size;
+    private final int size; // represents the size(Capacity) of the hashmap
     int hashTable[];
     int countTable[];
     int stateTable[];
 
-    Hashmap(final int size) {
+    public Hashmap(final int size) {
         this.size = size;
         hashTable = new int[size];
         countTable = new int[size];
         stateTable = new int[size];
+        
         Arrays.fill(countTable, 0);
+        Arrays.fill(stateTable, 0);
     }
-
-    public void insert() {
+    public Hashmap(final int[] Arr, final int size){
+        this.size = size;
+        hashTable = new int[size];
+        countTable = new int[size];
+        stateTable = new int[size];
+        
+        Arrays.fill(countTable, 0);
+        Arrays.fill(stateTable, 0);
+    }
+    
+    public void insert(int value) {
+        key = value % size;
         // implementation
     }
 
-    public void delete() {
+    public void delete(int value) {
         // implementation
     }
 
-    public void search() {
+    public boolean search(int value) {
         // implementation
     }
 
-    public void size() {
+    public int size() {
         // implementation
+      return this.size;
     }
 }
-
+// [1,2,3,4,5,6,7,8] 9,10,11,12,13
 public class Main {
-    public static void main(final String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        Hashmap myMap = new Hashmap(10); // Instantiate the Hashmap
+        myMap.insert();
+        myMap.search();
+        myMap.delete();
+        System.out.println("");
     }
 }
+
