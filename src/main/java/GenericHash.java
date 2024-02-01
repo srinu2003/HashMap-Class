d1a552518c4195d37c7df892b7462d39d726debb
srinu2003 <88500103+srinu2003@users.noreply.github.com>
2024-02-01 19:28:39 +0000

    Complete ✅

--- src/main/java/GenericHash.java
+++ src/main/java/GenericHash.java
@@ -1,43 +1,46 @@
 import java.util.Arrays;
 
-class opAHash {
+class openAddressingHash<T> {
     private final int size;
-    int DEFAULT_VALUE = 0;
-    int hashTable[];
+    T DEFAULT_VALUE = null;
+    T hashTable[];
     int countTable[];
     int stateTable[];
 
-    public opAHash(final int size) {
+    @SuppressWarnings("unchecked")
+    public openAddressingHash(final int size, final T defaultValue) {
         this.size = size;
-        hashTable = new int[size]; 
-        countTable = new int[size]; 
+        DEFAULT_VALUE = defaultValue;
+        hashTable = (T[]) new Object[size];
+        countTable = new int[size];
         stateTable = new int[size];
         Arrays.fill(countTable, 0);
         Arrays.fill(stateTable, 0);
     }
-    static int hashFunction(final int value, final int size){
+
+    static int hashFunction(final int key, final int size) {
         // TODO: Implement hash function
-        return value % size; //return Key
+        int hash = key % size;
+        return hash;
     }
-    public void insert(int value) {
-        // TODO: Implement value from being inserted if it is already in the hash table
+
+    public void insert(T value) {
         if (Arrays.stream(stateTable).sum() == size) {
             System.out.println("Hashmap is full");
             return;
         }
 
-        // TODO: Use hash function for key
-        int key = hashFunction(value, size);
+        int key = hashFunction(value.hashCode(), size);
         if (stateTable[key] == 0 || stateTable[key] == -1) {
             hashTable[key] = value;
             countTable[key]++;
             stateTable[key] = 1;
         } else if (stateTable[key] == 1) {
             while (stateTable[key] == 1) {
-                if (hashTable[key] == value) {
+                if (hashTable[key].equals(value)) {
                     break;
                 }
-                key = (key + 1) % size; // For linear probing
+                key = (key + 1) % size;
             }
             hashTable[key] = value;
             countTable[key]++;
@@ -45,8 +48,8 @@ class opAHash {
         }
     }
 
-    public void delete(int value) {
-        if (noOfElements() == 0){
+    public void delete(T value) {
+        if (noOfElements() == 0) {
             System.out.println("Hashmap is empty");
             return;
         }
@@ -56,30 +59,31 @@ class opAHash {
             System.out.println("Value not found");
             return;
         } else {
-            hashTable[key] = DEFAULT_VALUE;
+            hashTable[key] = null;
             countTable[key]--;
             stateTable[key] = (countTable[key] == 0) ? -1 : 0;
         }
     }
 
-    public int search(int value) {
-        int key = hashFunction(value, size);
-        
+    public int search(T value) {
+        int key = hashFunction(value.hashCode(), size);
         if (stateTable[key] == 0) {
             System.out.println("Value not found");
             return -1;
-        } else if (stateTable[key] != 0){
-            if (hashTable[key] == value) {
+        } else if (stateTable[key] != 0) {
+            if (hashTable[key] != null && hashTable[key].equals(value)) {
                 return key;
             } else {
                 int keyInitial = key;
                 while (stateTable[key] != 0) {
-                    key = (key + 1) % size; // For linear probing
-                    if(key == keyInitial){
+                    key = (key + 1) % size;
+                    if (key == keyInitial) {
                         System.out.println("Value not found");
                         return -1;
                     }
-                    if (hashTable[key] == value) { return key; }
+                    if (hashTable[key] != null && hashTable[key].equals(value)) {
+                        return key;
+                    }
                 }
             }
         }
@@ -91,21 +95,21 @@ class opAHash {
     }
 }
 
-public class Main {
-
+public class GenericHash {
     public static void main(String[] args) {
 
-        opAHash myMap = new opAHash(10);
+        openAddressingHash<Integer> myMap = new openAddressingHash<Integer>(10, null);
         myMap.insert(7);
         myMap.insert(2);
         myMap.insert(17);
+        myMap.insert(5);
         myMap.insert(5);
         System.out.println(myMap.search(7));
         // myMap.delete(7);
         // myMap.search(7);
         myMap.delete(17);
-        System.out.println("myMap"+":"+Arrays.toString(myMap.hashTable));
-        System.out.println("countTable"+":"+Arrays.toString(myMap.countTable));
-        System.out.println("stateTable"+":"+Arrays.toString(myMap.stateTable));
+        System.out.println("myMap" + ":" + Arrays.toString(myMap.hashTable));
+        System.out.println("myMap" + ":" + Arrays.toString(myMap.countTable));
+        System.out.println("myMap" + ":" + Arrays.toString(myMap.stateTable));
     }
-}   
+}
